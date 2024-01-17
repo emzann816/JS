f20d86316c1b8c185da78707734181eb2b21da42
emzann816 <142023271+emzann816@users.noreply.github.com>
2024-01-17 18:43:37 +0800

    Add files via upload

---
+++ music-artistapi.js
@@ -0,0 +1,26 @@
+[
+  {  "id": "1",
+    "title": "When We Were Young",
+    "singer": "Adele",
+    "img": "https://hips.hearstapps.com/hmg-prod/images/adele-reveals-the-reason-she-s-started-therapy-again-1670917670.jpg",
+    "file": "https://github.com/emzann816/JS/tree/main/img/Adele_1.jpg"  },
+  
+  {  "id": "2",
+    "title": "All of Me",
+    "singer": "John Legend",
+    "img": "https://hips.hearstapps.com/hmg-prod/images/adele-reveals-the-reason-she-s-started-therapy-again-1670917670.jpg",
+    "file": "https://github.com/emzann816/JS/tree/main/img/Adele_1.jpg"  },
+
+  { "id": "3",
+    "title": "All of I ask",
+    "singer": "Adele",
+    "img": "https://hips.hearstapps.com/hmg-prod/images/adele-reveals-the-reason-she-s-started-therapy-again-1670917670.jpg",
+    "file": "https://github.com/emzann816/JS/tree/main/img/Adele_1.jpg"  },
+
+  { "id": "4",
+    "title": "Always Remember Us This Way",
+    "singer": "Lagy Gaga",
+    "img": "https://hips.hearstapps.com/hmg-prod/images/adele-reveals-the-reason-she-s-started-therapy-again-1670917670.jpg",
+    "file": "https://github.com/emzann816/JS/tree/main/img/Adele_1.jpg"  }
+
+  ]
